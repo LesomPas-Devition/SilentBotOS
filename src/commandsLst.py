f05def773d182ff902c1b53a065426238a8f29bc
LesomPas-Devition <awaphioryas@outlook.com>
2024-10-22 20:22:06 +0800

    Update commandsLst.py
    更新了有关于漂流瓶的功能

--- src/commandsLst.py
+++ src/commandsLst.py
@@ -3,20 +3,32 @@ from typing import Union
 from ulid import ULID
 from botpy.message import C2CMessage, GroupMessage, Message
 import json
+from UGC import UserGeneratedContent
+from random import choice
 
 class DRVCommands: # Determined ReturnValue Commands
     @staticmethod
     def cello(atEvent: dict):
+        if atEvent['content']['params'] == 'helpmode':
+            return (
+                "\n功能: 恭喜你发现了这一个彩蛋\n"
+                "语法: /cello"
+            )
         return "\n Ciallo～(∠・ω< )⌒☆"
 
     @staticmethod
     def userhelp(atEvent: dict):
-
+        if atEvent['content']['params'] == 'helpmode':
+            return "恭喜你发现第二个彩蛋:\n《在帮助中使用帮助》"
         helpText = (
-            "1. help: 寻求帮助信息(这个有用吗)."
-            "2. repeat: 重复群u输入功能."
-            "3. sign: 第一次使用时, 将自动注册一个新账号. 之后就是正常的登录功能."
-            "4. 我要改名: 字面意思"
+            "\n下面是一些关于使用指令的通用注意事项\n这一段文字的末尾将给出目前所有的指令\n"
+            "1.在命令名后加上'-h'可以获得有关该指令的功能语法和注意事项的说明(如果某一项没写会省略)\n"
+            "2.如果你要把带有空格的消息视为一个整体传入到参数中请用英文引号包裹文本, 否则可能会有语法错误\n"
+            "(其实如果你的文本很复杂的话，建议直接包括引号\n"
+            "目前所有的命令:\n"
+            "登陆类: /enroll, /sign /rename\n"
+            "正常类: /repeat, /DriftBottle或/漂流瓶\n"
+            "其他类: /switching, /AAPs"
         )
 
         return helpText
@@ -24,6 +36,11 @@ class DRVCommands: # Determined ReturnValue Commands
 class NormalCommands:
     @staticmethod
     def repeat(atEvent: dict):
+        if atEvent['content']['params'] == 'helpmode':
+            return (
+                "\n功能: 重复用户输入内容\n"
+                "语法: /repeat <重复信息>"
+            )
         return atEvent['content']['params'][0]
 
 
@@ -57,6 +74,11 @@ class SignCommands:
 
     @classmethod
     def sign(cls, atEvent: dict):
+        if atEvent['content']['params'] == 'helpmode':
+            return (
+                "\n功能: 签到打卡\n"
+                "语法: /sign [AdminPassword]"
+            )
         memberList = SignCommands._getMemberList()
         todaySign = SignCommands._ioJsonData(cls.signPath, 'r')
         MOI = atEvent['msgMOI']
@@ -94,6 +116,11 @@ class SignCommands:
 
     @classmethod
     def rename(cls, atEvent: dict):
+        if atEvent['content']['params'] == 'helpmode':
+            return (
+                "\n功能: 更改账户名称\n"
+                "语法: /rename <名称>"
+            )
         memberList = SignCommands._getMemberList()
         MOI = atEvent['msgMOI']
 
@@ -107,6 +134,11 @@ class SignCommands:
 
     @classmethod
     def enroll(cls, atEvent: dict):
+        if atEvent['content']['params'] == 'helpmode':
+            return (
+                "\n功能: 创建新账户(发布内容要用)\n"
+                "语法: /enroll"
+            )
         memberList = SignCommands._getMemberList()
         MOI = atEvent['msgMOI']
 
@@ -139,6 +171,17 @@ class SignCommands:
         return "成功以管理员身份登录"
 
 
+    @classmethod
+    def _isAdmin(cls, atEvent: dict) -> bool:
+        memberList = SignCommands._getMemberList()
+        MOI = atEvent["msgMOI"]
+
+        if MOI not in memberList["members"] or MemberList['members'][MOI]['admin'] is None:
+            return False
+
+        return MemberList['members'][MOI]['admin']['adminMode']
+
+    @staticmethod
     def _ioJsonData(path: str, mode: Union['r', 'w'], content=None):
         js = open(path, mode, encoding="utf-8")
         if mode == 'w':
@@ -149,6 +192,45 @@ class SignCommands:
 
     def _getMemberList():
         return SignCommands._ioJsonData('./Data/Member.json', 'r')
+
+
+class DriftBottleCommands:
+    @staticmethod
+    def main(atEvent: dict) -> None:
+        params = atEvent["content"]["params"]
+        if type(params) == tuple:
+            return "参数数量不对呢, 建议检查一下"
+        if params == "helpmode":
+            return (
+                "\n功能: 捡起或扔出漂流瓶\n"
+                "语法: /DriftBottle throw <内容>\n"
+                "     /DriftBottle pick\n"
+                "     /漂流瓶 扔出 <内容>\n"
+                "     /漂流瓶 捡起\n"
+                "注意事项: 建议在扔出漂流瓶时将内容用引号包裹.且只能保存一张图片"
+            )
+
+        if params[0] not in ["throw", "pick", "捡起", "扔出"]:
+            return "第一项参数不对呢, 使用'/漂流瓶 -h'查看帮助哦"
+
+        if params[0] == "pick" or params[0] == "捡起":
+            sea = SignCommands._ioJsonData("./Data/sea.json", "r")
+            try:
+                seakey = choice(list(sea.keys()))
+                seavalue = sea[seakey]
+            except IndexError:
+                return "大海上空无一物呢"
+            if atEvent["msgGOI"] not in seavalue["browsed"]:
+                seavalue["browsed"].append(atEvent["msgGOI"])
+                sea[seakey] = seavalue
+                SignCommands._ioJsonData("./Data/sea.json", "w", content=sea)
+                return seavalue["content"]["text"] + f'\n\n——{seavalue["time"]}'
+            else:
+                return "这次你还没有捡到瓶子哦, 再试一次也许就可以了哦"
+
+        if params[0] == "throw" or params[0] == "扔出":
+            UserGeneratedContent("DriftBottle", atEvent["msgMOI"], {"text": params[1], "url": []}, atEvent["time"]).SavaByDriftBottle(atEvent['msgGOI'])
+            return "你向大海中抛出了一个瓶子, 期待未来有机会有人能够捡到ta"
 
 
 class DirectedCommands:
@@ -178,6 +260,14 @@ class DirectedCommands:
 class SystemCommands:
     @staticmethod
     def switching(atEvent: dict):  # for SessionType is 2.5
+        if atEvent['content']['params'] == 'helpmode':
+            return (
+                "\n功能: 切换进程\n"
+                "语法: /switching <pid>\n"
+                "特殊说明:\n"
+                "1.私信中不可使用/switching\n"
+                "2.只能切换命令创建进程类型为2.5的进程"
+            )
         if type(atEvent["content"]["message"]) == C2CMessage:
             return "私信环境中不可使用/switching"
         from SilentBotOS import SilentBotOS, SessionProcess
@@ -187,13 +277,20 @@ class SystemCommands:
             process = SilentBotOS.PidRunning[ULID.from_str(pid)]
         except KeyError:
             return "输入内容有误或者进程不存在"
-        # if process.sessionType == 2.5:
-        SilentBotOS.SessionMember[MOI] = {ULID.from_str(pid)}
-        return "切换成功"
+        if process.sessionType == 2.5:
+            SilentBotOS.SessionMember[MOI] = {ULID.from_str(pid)}
+            return "切换成功"
 
     @staticmethod
     def allAvailableProcesses(atEvent: dict) -> str:
-
+        if atEvent['content']['params'] == 'helpmode':
+            return (
+                "\n功能: 给出当前所有的运行进程\n"
+                "语法: /AAPs\n"
+                "特殊说明:\n"
+                "1.AAPs为all available processes的缩写\n"
+                "2.该指令仅限管理员使用, 因为过量的进程运行列表可能占用聊天内容"
+            )
         def _print(dictionary, header: str) -> str:
             result = f"{header}\n"
             for i in dictionary.values():
